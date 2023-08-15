c717a0ec758b57fe265f7cfdb53075e8bacaca40
jechague <jechague@genexus.com>
2023-08-15 09:34:07 -0300

    Add dummy app project for integration testing

---
+++ Tests/IntegrationTests/UITestingSampleApp/UITestingSampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UITestingSampleApp
+//
+//  Created by José Echagüe on 8/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
