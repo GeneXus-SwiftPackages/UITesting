0513a712dd965bce0b5fc58e6a30b8805da85f98
Sebastian Rodriguez Sepe <57290911+SeSebastiansepe@users.noreply.github.com>
2025-03-05 08:29:02 -0300

    Fix issue 104123 (#16)
    * Fix issue 104123
    
    * Update GXUITestingAPI.swift
    
    Added `.scrollView` to `_allElementTypes` to maintain consistency with `_tapableElementTypes`.  
    Also adjusted indentation using tabs instead of spaces to respect the style of the code.

--- Sources/GXUITest/GXUITestingAPI.swift
+++ Sources/GXUITest/GXUITestingAPI.swift
@@ -862,6 +862,7 @@ fileprivate let _allElementTypes: Array<XCUIElement.ElementType> = [.textField,
 																	.checkBox,
 																	.switch,
 																	.segmentedControl,
+								    									.scrollView,
 																	.other]
 
 fileprivate let _tapableElementTypes: Array<XCUIElement.ElementType> = [.button,
@@ -874,6 +875,7 @@ fileprivate let _tapableElementTypes: Array<XCUIElement.ElementType> = [.button,
 																		.checkBox,
 																		.switch,
 																		.segmentedControl,
+																		.scrollView,
 																		.other]
 
 fileprivate let _textInputElementTypes: Array<XCUIElement.ElementType> = [.textField,
